f0cf9c89ea722ea3302fb03d8a21b31d9b411e04
Dan Pickens <72703981+DanielPickens@users.noreply.github.com>
2020-12-12 10:34:22 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSumBST(self, root: TreeNode) -> int:
+        res = 0
+        def traverse(root):
+            nonlocal res
+            if not root: return 1, 0, None, None # this subtree is empty
+
+            ls, l, ll, lr = traverse(root.left)
+            rs, r, rl, rr = traverse(root.right)
+
+            if ((ls == 2 and lr < root.val) or ls == 1) and ((rs == 2 and rl > root.val) or rs == 1):
+                size = root.val + l + r
+                res = max(res, size)
+                return 2, size, (ll or root.val), (rr or root.val)
+            return 0, None, None, None # this subtree is not a BST
+
+        traverse(root)
+        return res
